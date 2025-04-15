ce6fcd46e4838c5c59e5e379ff2f7213e87e16c2
401923emir <emir1609v@gmail.com>
2025-04-15 09:10:07 +0200

    QR

--- tickets.php
+++ tickets.php
@@ -3,13 +3,14 @@
 <head><title>Tickets Bestellen</title></head>
 <link rel="stylesheet" href="tickets.css">
 <body>
+  
+  <form action="bestel2.php" method="POST">
   <h2>Bestel je tickets</h2>
-  <form action="bestel.php" method="POST">
-    <label>Voornaam: <input type="text" name="name" required></label><br>
-    <label>Achternaam: <input type="text" name="name" required></label><br>
-    <label>Email: <input type="email" name="email" required></label><br>
-    <label>Leeftijd <input type="text" name="leeftijd" required></label><br>
-    <label>Waar kom je vandaan? <input type="text" name="location" required></label><br>
+    <label>Voornaam: <input type="text" name="name" value="naam" required></label><br>
+    <label>Achternaam: <input type="text" name="name" value="achternaam" required></label><br>
+    <label>Email: <input type="email" name="email" value="email@hotmail.com" required></label><br>
+    <label>Leeftijd <input type="text" name="leeftijd" value="22" required></label><br>
+    <label>Waar kom je vandaan? <input type="text" name="location" value="provincie" required></label><br>
     <label>Aantal tickets: <input type="number" name="tickets" min="1" value="1" required></label><br><br>
     <button type="submit">Bestel</button>
   </form>
